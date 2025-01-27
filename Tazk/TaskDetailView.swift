95fc5167644adc5cb8ab4e472abbea17169127fd
sxdny <67187745+sxdny@users.noreply.github.com>
2025-01-27 13:06:00 +0100

    new tasks view + checkboxes

---
+++ Tazk/TaskDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailView.swift
+//  Tazk
+//
+//  Created by Sidney Silva Braz de Oliveira on 1/27/25.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailView()
+}
---
+++ Tazk/TaskView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskView.swift
+//  Tazk
+//
+//  Created by Sidney Silva Braz de Oliveira on 1/27/25.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskView()
+}
